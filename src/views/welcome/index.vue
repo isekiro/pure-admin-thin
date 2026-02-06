8ef99bd008e3a5eb45c1ac59ee6c35ba32ec1564
isekiro <394070956@qq.com>
2026-02-06 16:49:26 +0800

    feat: 新增echarts

--- src/views/welcome/index.vue
+++ src/views/welcome/index.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-defineOptions({
-  name: "Welcome"
-});
+// import Echarts from "@/components/ReEcharts/index.vue";
+// import { useEcharts } from "./hooks";
+// const { option, option1 } = useEcharts();
 </script>
 
 <template>
   <h1>Soma-UI（非国际化版本）</h1>
+  <!-- <div>
+    <Echarts :option="option" />
+    <Echarts :option="option1" />
+  </div> -->
 </template>
